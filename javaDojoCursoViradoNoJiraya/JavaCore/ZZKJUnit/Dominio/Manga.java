84614b4598361d6798e9f38b519ea6ecd00d4e5c
Gabriel Carvalho de matos <gabrielcarvalhox10@gmail.com>
2025-02-07 15:16:51 -0300

    Merge remote-tracking branch 'origin/main'

--- javaDojoCursoViradoNoJiraya/JavaCore/ZZKJUnit/Dominio/Manga.java
+++ javaDojoCursoViradoNoJiraya/JavaCore/ZZKJUnit/Dominio/Manga.java
@@ -1,4 +1,9 @@
 package JavaCore.ZZKJUnit.Dominio;
 
+import java.util.Objects;
+
 public record Manga(String name, int epidodes) {
+    public Manga {
+        Objects.requireNonNull(name);
+    }
 }
